80d5791c5a78d238f84bae901eb2c370f2e1674d
cmarchena <sprintwithcarlos@pm.me>
2024-04-25 10:55:20 +0200

    version where product details are fetch everytime

--- src/ProductDetails.jsx
+++ src/ProductDetails.jsx
@@ -1,10 +1,27 @@
-import { useParams } from 'react-router-dom'
-export default function ProductDetails(){
-    const {id} = useParams()
-    return(
-        <div data-cy="product-item">
-            <h1>Product Details</h1>
-            <p>Product ID: {id}</p>
-        </div>
-    )
+import { useEffect, useState } from "react";
+import { useParams, Link } from "react-router-dom";
+export default function ProductDetails() {
+  const baseURL = "https://fakestoreapi.com";
+  const [product, setProduct] = useState([]);
+
+  const { id } = useParams();
+  async function fetchProductByID(id) {
+    const response = await fetch(`${baseURL}/products/${id}`);
+    const data = await response.json();
+    setProduct(data);
+  }
+  useEffect(() => {
+    fetchProductByID(id);
+  }, []);
+  return (
+    <div data-cy="product-item" className="product-item">
+      <h2>Product Details</h2>
+      <p>Product ID: {product.id}</p>
+      <p>{product.description}</p>
+      <img src={product.image} alt={product.title} />
+      <p>Category: {product.category}</p>
+      <p>Price: {product.price}</p>
+      <Link to="/products">Go back</Link>
+    </div>
+  );
 }
--- src/ProductsList.jsx
+++ src/ProductsList.jsx
@@ -1,33 +1,50 @@
+import { useEffect } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
 export default function ProductsList() {
-    // generate an array of products where product is and object with id, name and price
-    const products = [
-        {
-            id: 1,
-            name: 'Product 1',
-            price: 10
-        },
-        {
-            id: 2,
-            name: 'Product 2',
-            price: 20
-        },
-        {
-            id: 3,
-            name: 'Product 3',
-            price: 30
-        }
-    ];
+  // Now instead of hardcoding the products, we will fetch them from the API
+  const baseURL = "https://fakestoreapi.com";
+  /* 
+  An example of what we do get from the API:
+  [
+    {
+    "id": 1,
+    "title": "Fjallraven - Foldsack No. 1 Backpack, Fits 15 Laptops",
+    "price": 109.95,
+    "description": "Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday",
+    "category": "men's clothing",
+    "image": "https://fakestoreapi.com/img/81fPKd-2AYL._AC_SL1500_.jpg",
+    "rating": { "rate": 3.9, "count": 120 }
+    }
+  ] */
 
-    return (
-        <div data-cy="product-list">
-            {products.map(product => (
-                <div key={product.id}>
-                    <h2>{product.name}</h2>
-                    <p>Price: {product.price}</p>
-                    <Link to={`/products/${product.id}`}>Go to details</Link>
-                </div>
-            ))}
+  const [products, setProducts] = useState([]);
+
+  //   useEffect(() => {
+  //     fetch(`${baseURL}/products`)
+  //       .then((response) => response.json())
+  //       .then((data) => setProducts(data));
+  //   }, []);
+  async function fetchAllProducts() {
+    const response = await fetch(`${baseURL}/products`);
+    const data = await response.json();
+    console.log({ data });
+    setProducts(data);
+  }
+  useEffect(() => {
+    fetchAllProducts();
+  }, []);
+
+  return (
+    <div data-cy="product-list" className="product-list">
+      {products.map((product) => (
+        <div key={product.id} className="product-card">
+          <img src={product.image} alt={product.title} />
+          <p className="product-title">{product.title}</p>
+          <p>Price: {product.price}</p>
+          <Link to={`/products/${product.id}`}>Go to details</Link>
         </div>
-    )
+      ))}
+    </div>
+  );
 }
